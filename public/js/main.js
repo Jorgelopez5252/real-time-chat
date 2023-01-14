57ff989710a936634ff7429d128a894e2c19554c
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2023-01-14 11:44:11 -0700

    next step is to implement the rooms and users

--- public/js/main.js
+++ public/js/main.js
@@ -32,9 +32,10 @@ function outputMessage(message) {
 
 const div = document.createElement('div');
 div.classList.add('message');
-div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+// dot notation being added since message is now an object
+div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
 <p class="text">
-    ${message}
+    ${message.text}
 </p>`;
 document.querySelector('.chat-messages').appendChild(div);
 }

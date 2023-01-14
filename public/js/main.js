fe2b85840bdc1fdfa66bde648914fa0502c995e4
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2023-01-13 18:28:26 -0700

    issues with createElement solved

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,10 @@ const chatForm = document.getElementById('chat-form');
 
 const socket = io();
 
-
+//message from server
 socket.on('message', message => {
     console.log(message)
+    outputMessage(message);
 })
 
 //Message submit
@@ -17,3 +18,15 @@ chatForm.addEventListener('submit', (e) => {
     //emit message to server
     socket.emit('chatMessage', msg);
 });
+
+// output message to dom with DOM Manipulation
+function outputMessage(message) {
+
+const div = document.createElement('div');
+div.classList.add('message');
+div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+<p class="text">
+    ${message}
+</p>`;
+document.querySelector('.chat-messages').appendChild(div);
+}

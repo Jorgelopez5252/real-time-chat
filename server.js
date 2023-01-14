b4714e3c97629277528b42929e278e862e46ae3c
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2023-01-13 17:30:57 -0700

    smalll update to keep app safe from crashing

--- server.js
+++ server.js
@@ -12,7 +12,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when a client connects
 io.on('connection', socket => {
-    console.log('New WS Connection..')
+
+    socket.emit('message', 'Welcome to Speak up');
+
+    // notify when a user connects
+    socket.broadcast.emit('message', 'A user has Joined the chat');
+
+    // Notfies when client leaves
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat')
+    });
 });
 
 const PORT = 3000 || process.env.PORT;

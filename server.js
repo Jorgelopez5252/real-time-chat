d2d03f1446c8d320f3f584d797129297ac23abbd
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2023-01-13 17:21:04 -0700

    moving on to chat html

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const io = socketio(server);
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Run when a client connects
+io.on('connection', socket => {
+    console.log('New WS Connection..')
+});
+
 const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));

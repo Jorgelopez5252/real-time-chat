cdd38f9a6440834b783c412ad06a5d59f51588a9
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2023-01-14 12:16:15 -0700

    created user.js file

--- server.js
+++ server.js
@@ -1,35 +1,42 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages')
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 const io = socketio(server);
 
 //Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const botName = 'ChatCord Bot';
+const botName = "ChatCord Bot";
 
 // Run when a client connects
-io.on('connection', socket => {
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    
 
-    socket.emit('message', formatMessage(botName , 'Welcome to ChatCord!'));
+    //welcome new user
+    socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
 
-    // notify when a user connects
-    socket.broadcast.emit('message', formatMessage(botName , 'A user has Joined the chat'));
+    // brodcast when a user connects
+    socket.broadcast.emit(
+      "message",
+      formatMessage(botName, "A user has Joined the chat")
+    );
+  });
 
-    // Notfies when client leaves
-    socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName , 'A user has left the chat'))
-    });
+  //listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", formatMessage("USER", msg));
+  });
 
-    //listen for chatMessage
-    socket.on('chatMessage', msg => {
-        io.emit('message',formatMessage('USER',  msg))
-    })
+  // Notfies when client leaves
+  socket.on("disconnect", () => {
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
